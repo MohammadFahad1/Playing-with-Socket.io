87381ffc027382529a6f8be949af895edcd1f7b2
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-05-12 23:14:50 +0600

    Client server simple communication done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 app = express();
 server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the playing with socket.io!");
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
 });
 
 // Global Error Handler
